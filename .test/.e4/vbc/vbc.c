c2a384283167f2d1351bb1d62e316921918fd160
Attila Nemet <attila.nemet@gmail.com>
2025-09-23 07:56:22 +0200

    vbc1

--- .test/.e4/vbc/vbc.c
+++ .test/.e4/vbc/vbc.c
@@ -5,272 +5,276 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anemet <anemet@student.42luxembourg.lu>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/19 13:09:26 by anemet            #+#    #+#             */
-/*   Updated: 2025/09/19 17:30:00 by anemet           ###   ########.fr       */
+/*   Created: 2025/09/22 21:05:50 by anemet            #+#    #+#             */
+/*   Updated: 2025/09/23 07:53:21 by anemet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+
+
+Assignment name  : vbc
+Expected files   : *.c *.h
+Allowed functions: malloc, calloc, realloc, free, printf, isdigit, write
+--------------------------------------------------------------------------------
+
+Write a program that will print the result of a math expression given as
+argument.
+You must handle the operations + * and the parenthesis.
+You don't have to handle whitespaces in the expression.
+All the values in the expression will be between 0 and 9 included.
+In case of unexpected symbol or inappropriate parenthesis, you will
+print "Unexpected token '%c'\n" and exit with the code 1 (if the
+symbol is the end of input you will print: "Unexpected end of input\n").
+In case of a syscall failure you will just exit with the code 1.
+
+You will find in this directory the beginning of the code you need to write.
+
+For example this should work:
+$> ./vbc '1' | cat -e
+1$
+$> ./vbc '2+3' | cat -e
+5$
+$> ./vbc '3*4+5' | cat -e
+17$
+$> ./vbc '3+4*5' | cat -e
+23$
+$> ./vbc '(3+4)*5' | cat -e
+35$
+$> ./vbc '(((((2+2)*2+2)*2+2)*2+2)*2+2)*2' | cat -e
+188$
+$> ./vbc '1+' | cat -e
+Unexpected end of input$
+$> ./vbc '1+2)' | cat -e
+Unexpected token ')'$
+$> ./vbc '1+2+3+4+5' | cat -e
+15$
+$> ./vbc '2*4+9+3+2*1+5+1+6+6*1*1+8*0+0+5+0*4*9*5*8+9*7+5*1+3+1+4*5*7*3+0*3+4*8+8+8+4*0*5*3+5+4+5*7+9+6*6+7+9*2*6*9+2+1*3*7*1*1*5+1+2+7+4+3*4*2+0+4*4*2*2+6+7*5+9+0+8*4+6*7+5+4*4+2+5*5+1+6+3*5*9*9+7*4*3+7+4*9+3+0+1*8+1+2*9*4*5*1+0*1*9+5*3*5+9*6+5*4+5+5*8*6*4*9*2+0+0+1*5*3+6*8*0+0+2*3+7*5*6+8+6*6+9+3+7+0*0+5+2*8+2*7*2+3+9*1*4*8*7*9+2*0+1*6*4*2+8*8*3*1+8+2*4+8*3+8*3+9*5+2*3+9*5*6*4+3*6*6+7+4*8+0+2+9*8*0*6*8*1*2*7+0*5+6*5+0*2+7+2+3+8*7+6+1*3+5+4*5*4*6*1+4*7+9*0+4+9*8+7+5+6+2+6+1+1+1*6*0*9+7+6*2+4*4+1*6*2*9+3+0+0*1*8+4+6*2+6+2*7+7+0*9+6+2*1+6*5*2*3*5*2*6*4+2*9*2*4*5*2*2*3+8+8*3*2*3+0*5+9*6+8+3*1+6*9+8+9*2*0+2' | cat -e
+94305$
+$> ./vbc '(1)' | cat -e
+1$
+$> ./vbc '(((((((3)))))))' | cat -e
+3$
+$> ./vbc '(1+2)*3' | cat -e
+9$
+$> ./vbc '((6*6+7+5+8)*(1+0+4*8+7)+2)+4*(1+2)' | cat -e
+2254$
+$> ./vbc '((1+3)*12+(3*(2+6))' | cat -e
+Unexpected token '2'$
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-// defining the struct of a node in our AST (Abstract Syntax Tree)
-typedef struct node
-{
-	// enum representing the type of the node
-	// it can be addition/multiplication/numerical value
-	enum
-	{
-		ADD,	// represents the '+' operation
-		MULTI,	// represents the '*' operation
-		VAL,	// represents a single digit value (0..9)
-	}	type;
-	int val;		// if the node type is VAL, this will hold the number
-	struct node *l;	// pointer to the left child node (for type ADD and MULTI)
-	struct node *r;	// pointer to the right child node (for type ADD and MULTI)
-}	node;
+typedef struct node {
+    enum {
+        ADD,
+        MULTI,
+        VAL
+    }   type;
+    int val;
+    struct node *l;
+    struct node *r;
+}   node;
 
-/* new_node()
-	A helper function to create a new node on the heap
-	- it takes a 'node' struct as parameter
-	- allocates memory for it
-	- copies the contents of the parameter `n` (data on stack) to `*ret` (on heap)
-	Returns the pointer `ret` to the newly allocated node on the heap
-*/
-node *new_node(node n)
+node    *new_node(node n)
 {
-	// allocate memory for one 'node' on heap (initialized to zero)
-	node *ret = calloc(1, sizeof(n));
-	// subject requirement:
-	// In case of a syscall failure just exit with the code 1
-	// (this should free up all allocated memory to the process)
-	if (!ret)
-		exit(1);
-	// Copy the data from the provided node 'n' (on stack) to new node (on heap)
-	*ret = n;
-	return ret;
+    node *ret = calloc(1, sizeof(n));
+    if (!ret)
+        // return (NULL);
+        exit(1); // subject description allows this
+    *ret = n;
+    return (ret);
 }
 
-/* destroy_tree()
-	Recursively frees the memory allocated for the AST
-*/
-void	destroy_tree(node *n)
+void    destroy_tree(node *n)
 {
-	// base case: if the node is NULL, there's nothing to free, just return
-	if (!n)
-		return;
-	// if the node is an operation (not a value), we free its children first
-	if (n->type != VAL)
-	{
-		destroy_tree(n->l);
-		destroy_tree(n->r);
-	}
-	// Finally free the node itself
-	free(n);
+    if (!n)
+        return ;
+    if (n->type != VAL)
+    {
+        destroy_tree(n->l);
+        destroy_tree(n->r);
+    }
+    free(n);
 }
 
-/* unexpected()
-	Prints an error message for an unexpected token
-*/
-void unexpected(char c)
+void    unexpected(char c)
 {
-	if (c)
-		printf("Unexpected token '%c'\n", c);
-	else
-		// if 'c' is the NULL terminator => we reached the end unexpectedly
-		printf("Unexpected end of input");
+    if (c)
+        printf("Unexpected token '%c'\n", c);
+    else
+        printf("Unexpected end of input\n");
 }
 
-// Forward declarations for the parsing functions.
-// This is necessary because the functions call each other recursively
+int accept(char **s, char c)
+{
+    if (**s == c)
+    {
+        (*s)++;
+        return (1);
+    }
+    return (0);
+}
+
+int expect(char **s, char c)
+{
+    if (accept(s, c))
+        return (1);
+    unexpected(**s);
+    return (0);
+}
+
+
+// forward definition of parse_expr()
 node *parse_expr(char **s);
-node *parse_term(char **s);
-node *parse_factor(char **s);
+
 
 /* parse_factor()
-	This functions parses a "factor"
-	In our grammar a factor is either a single number of an expression in parentheses.
-	This is the highest level of precedence
+    - a number or an expression in parenthesis
 */
 node *parse_factor(char **s)
 {
-	// If the current character is a digit
-	if (isdigit(**s))
-	{
-		// ...create a new node of type VAL.
-		// The value is the character converted to an integer
-		// We then advance the string pointer to the next character
-		return new_node((node){VAL, *(*s)++ - '0', NULL, NULL});
-	}
-	// If the current character is an opening parenthesis...
-	if (**s == '(')
-	{
-		// ...consume the '('
-		(*s)++;
-		// ...and recursively parse the expression inside the parentheses.
-		node *ret = parse_expr(s);
-		// After the inner expression, we expect a closing parenthesis
-		if (**s != ')')
-		{
-			// If we don't find a ')', it's an error
-			unexpected(**s);
-			destroy_tree(ret); // clean up partial tree
-			return NULL;
-		}
-		// consume the ')'
-		(*s)++;
-		return ret;
-	}
-	// If it's neither a digit nor '(', it's an unexpected token
-	unexpected(**s);
-	return NULL;
+    if (isdigit(**s))
+    {
+        // return new VAL node with the digit value
+        node *ret = new_node((node){VAL, **s - '0', NULL, NULL});
+        (*s)++;
+        return ret;
+    }
+    // if opening parenthesis -> evaluate expression
+    if (**s == '(')
+    {
+        // consume '('
+        (*s)++;
+        // parse expression
+        node *ret = parse_expr(s);
+        // check closing ')'
+        if (**s != ')')
+        {
+            // unexpected(**s);   unexpected wi
+            destroy_tree(ret);
+            return NULL;
+        }
+        // consume ')'
+        (*s)++;
+        return ret;
+    }
+    // if neither digit , neither opening parenthesis
+    unexpected(**s);
+    return NULL;
 }
 
-/*            Let's examine *(*s)++
-
-Step 1: Parsing with Precedence Rules
-	- The compiler sees two operators trying to act on the operand (*s):
-		the prefix * and the postfix ++.
-	- The precedence table says: postfix ++ > prefix *.
-	- Because of this, the compiler groups the expression as written with parentheses:
-		*( (*s)++ )
-	- This grouping means:
-	"The dereference operator * will be applied to the result of the (*s)++ operation."
-
-Step 2: Runtime Evaluation
-	- Now the program has to execute *( (*s)++ ).
-		To do that, it must first figure out the result of the inner part, (*s)++.
-	- The postfix ++ operator evaluates to the variable's original value,
-		and the increment happens as a side effect after the original value has been taken.
-
-Conclusion
-	- Your observation was that the dereference effectively happens before the increment.
-		You are right.
-	- Your conclusion was that this must mean * has higher precedence.
-		This is the subtle error.
-	- The reality is:
-		++ has higher precedence, so it binds to (*s).
-		The * is applied to the result of the ++ expression.
-		But the special evaluation rule for postfix ++ is
-		that its result is the value before it increments, which creates the behavior you saw.
-*/
 
 /* parse_term()
-	This function parses a "term"
-	In our grammar a term is a sequence of factors mltiplied togheter
-	This handles the '*' operation.
+    - terms: series of factors multiplied together (handling '*')
+    - factor: a number or an expression in parenthesis
+    Return NULL on error
 */
 node *parse_term(char **s)
 {
-	// A term must start with a factor
-	node *ret = parse_factor(s);
-	if (!ret)
-		return NULL;
+    // must start with a factor
+    node *ret = parse_factor(s);
+    if (!ret)
+        return NULL;
 
-	// After the first factor, we loop as long as we see multiplication operators.
-	while (**s == '*')
-	{
-		// Consume the '*'
-		(*s)++;
-		// The right-hand side of the multiplication must be another factor
-		node *right = parse_factor(s);
-		if (!right)
-		{
-			destroy_tree(ret);  // Clean up on error
-			return NULL;
-		}
-		// Create a new MULTI node. The current 'ret' becomes the left child,
-		// and the newly parsed 'right' becomes the right child.
-		ret = new_node((node){MULTI, 0, ret, right});
-	}
-	return ret;
+    // after first factor we loop until we see '*' operators
+    while (**s == '*')
+    {
+        // consume '*'
+        (*s)++;
+        node *right = parse_factor(s);
+        if (!right)
+        {
+            destroy_tree(ret);
+            return NULL;
+        }
+        // create MULTI node with (ret, right)
+        ret = new_node((node){MULTI, 0, ret, right});
+    }
+    return ret;
 }
 
+
 /* parse_expr()
-	This function parses and "expression".
-	In our grammar the expression is a sequence of terms added together.
-	This handles the '+' operation. This is the lowest level of precedence.
+    - expr: series of terms added together (handling '+'), lowest level of precedence
+    - terms: series of factors multiplied together (handling '*')
+    - factor: a number or an expression in parenthesis
+    Return NULL on error
 */
 node *parse_expr(char **s)
 {
-	// An expression must start with a term.
-	node *ret = parse_term(s);
-	if (!ret)
-		return NULL;
+    // must start with a term
+    node *ret = parse_term(s);
+    if (!ret)
+        return NULL;
 
-	// After the first term, we loop as long as we see addition operators
-	while (**s == '+')
-	{
-		// Consume the '+'
-		(*s)++;
-		// The right-hand side of the addition must be another term
-		node *right = parse_term(s);
-		if (!right)
-		{
-			destroy_tree(ret);
-			return NULL;
-		}
-		// Create a new ADD node. The current 'ret' becomes the left child,
-		// and the newly parsed 'right' becomes the right child.
-		ret = new_node((node){ADD, 0, ret, right});
-	}
-	return ret;
+    // after the 1st term we loop until we see '+' operators
+    while (**s == '+')
+    {
+        // consume '+'
+        (*s)++;
+        node *right = parse_term(s);
+        if (!right)
+        {
+            destroy_tree(ret);
+            return NULL;
+        }
+        // create new ADD node with (ret, right)
+        ret = new_node((node){ADD, 0, ret, right});
+    }
+    return ret;
 }
 
-/* parse_main()
-	This is the main parsing function that kicks off the process
-*/
-node *parse_main(char *s)
+
+
+
+
+
+node    *parse_main(char *s)
 {
-	// Start by parsing the entire expression
-	node *ret = parse_expr(&s);
-	if (!ret)
-		return NULL;
+    node *ret = parse_expr(&s);
 
-	// After parsing, we should be at the end of the string.
-	// If there are any characters left, it's an error
-	if (*s)
-	{
-		unexpected(*s);
-		destroy_tree(ret);
-		return NULL;
-	}
-	return ret;
+    // if s is not pointing the the end of the string '\0'
+    // this means parsing failed, return NULL
+    if (*s)
+    {
+        unexpected(*s);
+        destroy_tree(ret);
+        return (NULL);
+    }
+    return (ret);
 }
 
-// This function evaluates the result of the expression represented by the AST
-// It does this by recursively traversing the tree
+
+
+
+
 int eval_tree(node *tree)
 {
-	switch (tree->type)
-	{
-		// if the node is ADD, evaluate left and right and add the results
-		case ADD:
-			return (eval_tree(tree->l) + eval_tree(tree->r));
-		// if node is MULTI, evaluate left and right and multiply the results
-		case MULTI:
-			return (eval_tree(tree->l) * eval_tree(tree->r));
-		// if node is VAL, simply return the value. This is the base case for recursion.
-		case VAL:
-			return (tree->val);
-	}
-	return 0; // ideally this shouldn't be reached
+    switch (tree->type)
+    {
+        case ADD:
+            return (eval_tree(tree->l) + eval_tree(tree->r));
+        case MULTI:
+            return (eval_tree(tree->l) * eval_tree(tree->r));
+        case VAL:
+            return (tree->val);
+    }
+    return 0; // this shouldn't be reached in an ideal world
 }
 
-// The main() function
 int main(int argc, char **argv)
 {
-	// The program expects exactly one argument: the mathematical expression
-	if (argc != 2)
-		return 1;
-	// Call the main parsing function to build the AST from the command line argument
-	node *tree = parse_main(argv[1]);
-	// If parsing fails, parse_main will return NULL and have already printed an error
-	if (!tree)
-		return 1;
-	// If parsing is successful, evaluate the tree and print the result
-	printf("%d\n", eval_tree(tree));
-	// Free AST tree
-	destroy_tree(tree);
-	return 0;
+    if (argc != 2)
+        return (1);
+    node *tree = parse_main(argv[1]);
+    if (!tree)
+        return (1);
+    printf("%d\n", eval_tree(tree));
+    destroy_tree(tree);
 }
+
+
